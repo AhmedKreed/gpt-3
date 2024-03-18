bead70cc0c8a21cd6c6e8991204ed39899a6e049
Ahmed hussen <149872862+AhmedKreed@users.noreply.github.com>
2024-03-19 00:48:21 +0200

    Update WhatGPT3.tsx
    adding the html element (page) that has been created in page.tsx

--- src/containers/WhatGPT3.tsx
+++ src/containers/WhatGPT3.tsx
@@ -1,5 +1,11 @@
 import Feature from "@/components/Feature";
-
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      page: any;
+    }
+  }
+}
 const WhatGPT3 = () => {
   const features = [
     {
@@ -16,40 +22,46 @@ const WhatGPT3 = () => {
     },
   ];
   return (
-    <section id="What is GPT?" className="w-full sm:section mb-52">
-      <div className="p-10 gpt3__whatgpt3 flex flex-col gap-28">
-        <div className="flex justify-between gap-5 flex-wrap pt-3">
-          <Feature
-            title={"What is GPT-3"}
-            text={
-              "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
-            }
-            font={true}
-          />
-        </div>
-        <div className="flex justify-between items-center flex-wrap">
-          <h1 className="text-gradient font-extrabold text-3xl sm:text-[34px] max-w-[470px]">
-            The possibilities are beyond your imagination
-          </h1>
-          <p
-            className="text-sub-text font-medium leading-8 max-md:w-full "
-            style={{ direction: "rtl" }}
-          >
-            Explore The Library
-          </p>
-        </div>
-        <div className="flex items-center justify-center gap-11 max-lg:flex-wrap max-md:flex-col">
-          {features.map((feature) => (
-            <div
-              key={feature.title}
-              className="flex flex-col gap-12 max-lg:basis-[45%] "
+    <page id="What is GPT?" name="What is GPT?">
+      <section className="w-full sm:section mb-52">
+        <div className="p-10 gpt3__whatgpt3 flex flex-col gap-28">
+          <div className="flex justify-between gap-5 flex-wrap pt-3">
+            <Feature
+              title={"What is GPT-3"}
+              text={
+                "We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+              }
+              font={true}
+            />
+          </div>
+          <div className="flex justify-between items-center flex-wrap">
+            <h1 className="text-gradient font-extrabold text-3xl sm:text-[34px] max-w-[470px]">
+              The possibilities are beyond your imagination
+            </h1>
+            <p
+              className="text-sub-text font-medium leading-8 max-md:w-full "
+              style={{ direction: "rtl" }}
             >
-              <Feature title={feature.title} text={feature.text} font={true} />
-            </div>
-          ))}
+              Explore The Library
+            </p>
+          </div>
+          <div className="flex items-center justify-center gap-11 max-lg:flex-wrap max-md:flex-col">
+            {features.map((feature) => (
+              <div
+                key={feature.title}
+                className="flex flex-col gap-12 max-lg:basis-[45%] "
+              >
+                <Feature
+                  title={feature.title}
+                  text={feature.text}
+                  font={true}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </page>
   );
 };
 
